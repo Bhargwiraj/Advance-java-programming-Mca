46458e68f17586490b494f3c2424ab38a09defcc
Bhargwi <bhargwishrivastava@gmail.com>
2026-04-08 00:46:04 +0530

    Day 3

---
+++ Day2/Main2.java
@@ -0,0 +1,8 @@
+package Day2;
+ import java.util.*;
+public class Main2 {
+    public static void main(String[] args) {
+        List<String>list=Arrays.asList("Java", "Python", "C" );
+        list.stream().map(str->str.charAt(0)).forEach(System.out::println);
+    }
+}
