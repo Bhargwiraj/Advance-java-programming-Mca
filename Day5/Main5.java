ae89761db08698c161c90125895d747c0b79396c
Bhargwi <bhargwishrivastava@gmail.com>
2026-04-11 00:24:39 +0530

    Day 5

---
+++ Day5/Main5.java
@@ -0,0 +1,8 @@
+package Day5
+import java.util.*;
+public class Main5 {
+    public static void main(String[] args) {
+        List <String> list = Arrays .asList("Java", "Stream", "API");
+        list.stream().map(x->x.length()).forEach(System.out::println);
+    }
+}
