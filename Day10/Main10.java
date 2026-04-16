551f34786821a7ebcd4f46d70d3b2203431e0bfc
Bhargwi <bhargwishrivastava@gmail.com>
2026-04-17 01:52:48 +0530

    Day 11

--- Day10/Main10.java
+++ Day10/Main10.java
@@ -4,7 +4,7 @@ package Day10;
 import java.util.*;
 public class Main10 {
     public static void main(String[] args) {
-        List<String> list= Arrays.asList("Aman","Mothi","Rahul","Amit");
+        List<String> list=  Arrays.asList("Aman","Mothi","Rahul","Amit");
         List<String> resultstring=list.stream().filter(x->x.startsWith("A")).toList();
         System.out.println(resultstring);
     }
